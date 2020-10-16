b5fbb13b82c37c141f927ec3e37eb3253888c5f5
Viktor Birgander <viktorbirgander@gmail.com>
2020-10-16 09:32:07 +0200

    test commit

--- CS1 Projekt OOP2/CS1 Projekt OOP2/Program.cs
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Program.cs
@@ -14,6 +14,7 @@ namespace CS1_Projekt_OOP2
         [STAThread]
         static void Main()
         {
+            //test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
