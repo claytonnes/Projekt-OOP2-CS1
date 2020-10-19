1e6cd027b50ee53389ed126481a34180c5c5ff37
naen8918 <nazgul.engvall@gmail.com>
2020-10-19 17:28:16 +0200

    CustomerClassUpdate1

--- CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Customer.cs
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,77 @@ namespace CS1_Projekt_OOP2
 {
     public class Customer
     {
-        public int Number { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; } //hej 
-        
+        public int _number;    // set value is unique and non-negative
+        public string _name;   // non-empty value
+        public string _phone;  // non-empty value
+        public string _email;  // non-empty value
+
+        public Customer()
+        {
+          
+        }
+
+        public int Number 
+        {
+            get { return _number; }
+            set { _number = (value < 0) ? throw new ArgumentOutOfRangeException() : value; } 
+        }
+
+        public string Name 
+        {
+            get { return _name; }
+
+            set {
+                if (_name == null || _name == "")
+                    throw new NullValueException(" Name cannot be null ");
+                else
+                        _name = value;
+            }
+        }
+
+        public string Phone 
+        {
+            get { return _phone; }
+
+            set
+            {
+                if (_phone == null || _phone == "")
+                    throw new NullValueException(" Phone cannot be null ");
+                else
+                    _phone = value;
+            }
+        } 
+        public string Email 
+        {
+            get { return _email; }
+
+            set
+            {
+                if (_email == null || _email == "")
+                    throw new NullValueException(" Email cannot be null ");
+                else
+                    _email = value;
+            }
+        }
+    }
+
+    [Serializable]
+    internal class NullValueException : Exception
+    {
+        public NullValueException()
+        {
+        }
+
+        public NullValueException(string message) : base(message)
+        {
+        }
+
+        public NullValueException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NullValueException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
