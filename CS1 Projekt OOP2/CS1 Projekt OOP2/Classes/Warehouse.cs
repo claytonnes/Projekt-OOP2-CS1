8d6ee70073f0bbf96ea7ef979b440d37b9666cd0
Viktor Birgander <viktorbirgander@gmail.com>
2020-10-19 13:48:29 +0200

    Lagt till data layer (Warehouse.cs) och tillhörande interface IWarehouse

---
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Warehouse.cs
@@ -0,0 +1,14 @@
+﻿using CS1_Projekt_OOP2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1_Projekt_OOP2.Classes
+{
+    class Warehouse : IWarehouse
+    {
+
+    }
+}
