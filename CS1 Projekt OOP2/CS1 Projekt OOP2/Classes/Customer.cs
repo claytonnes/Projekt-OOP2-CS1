36b3048a56dc50aab599bbf96a120e76fb37e514
Mooanyy <50822532+Mooanyy@users.noreply.github.com>
2020-10-19 15:05:42 +0200

    hej

--- CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Customer.cs
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Customer.cs
@@ -11,6 +11,7 @@ namespace CS1_Projekt_OOP2
         public int Number { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } //hej 
+        
     }
 }
