9abd824bedda0b1cac7d31b2b23d24f08f43494c
kvacke <jens.bleckert@gmail.com>
2020-10-19 15:37:08 +0200

    Implemented Product class

--- CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Product.cs
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Classes/Product.cs
@@ -8,11 +8,45 @@ namespace CS1_Projekt_OOP2
 {
     public class Product
     {
-        int Code { get; set; }
-        string Name { get; set; }
-        double Price { get; set; }
-        int Stock { get; set; }
-        DateTime FirstAvailable { get; set; }
-        DateTime NextStocking { get; set; }
+        private int _code; //får inte vara mindre än 0
+        private string _name; // får inte vara null or empty
+        private double _price; //får inte vara mindre än 0
+        private int _stock; // får inte vara mindre än 0
+
+        public Product()
+        {
+
+        }
+
+        public int Code
+        {
+            get { return _code; }
+            set { _code = (value < 0) ? throw new ArgumentOutOfRangeException() : value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = (value < 0) ? throw new ArgumentOutOfRangeException() : value; }
+        }
+        public int Stock
+        {
+            get { return _stock; }
+            set { _stock = (value < 0) ? throw new ArgumentOutOfRangeException() : value; }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                    if (value == "" || value == null) throw new ArgumentException();
+                    else _name = value;
+                }
+        }
+        
+        public DateTime FirstAvailable { get; set; }
+        public DateTime NextStocking { get; set; }
+
     }
 }
