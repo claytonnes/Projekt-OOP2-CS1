5d9329068f1e1c97108bca06391146694d333e23
kvacke <jens.bleckert@gmail.com>
2020-10-29 13:23:52 +0100

    Final comments

--- CS1 Projekt OOP2/CS1 Projekt OOP2/Program.cs
+++ CS1 Projekt OOP2/CS1 Projekt OOP2/Program.cs
@@ -8,6 +8,26 @@ using System.Windows.Forms;
 
 namespace CS1_Projekt_OOP2
 {
+    /*
+     
+    Welcome to the Warehouse Control Panel!
+    This program simulates the creation and management of some commercial organizations customers, products and orders.
+
+    Already added is some sample data for testing different outcomes of the batch-processing method of orders (Press "Process Orders")
+    found in the Warehouse class. Where orders are displayed in a datagridview they can be clicked to open the
+    ViewOrder form which shows additional information.
+
+    Orders may be imported by placing a .json file in the bin/Debug/neworders folder.
+    The Warehouse class handles this import under the region "Utility methods".
+    An example JSON file can be found in the folder "Example JSON" in the project folder.
+    
+    Thanks for a fun project!
+
+    Moa & Nazgul & Victor & Jens
+
+     */
+
+
     static class Program
     {
         /// <summary>
@@ -17,6 +37,7 @@ namespace CS1_Projekt_OOP2
         static void Main()
         {
             IWarehouse wh = new Warehouse();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FRMMain(wh));
